7098bc59b0fc47ddf83a6cbe9c6780583ad3bf71
Márcio de Andrade <gmandrade1@hotmail.com>
2022-12-28 14:28:10 -0400

    Add project files.

---
+++ RabbitMQProductApi/RabbitMQ/RabitMQProducer.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+
+namespace RabbitMQProductApi.RabbitMQ
+
+{
+    public class RabitMQProducer : IRabitMQProducer
+    {
+        public void SendProductMessage<T>(T message)
+        {
+            //aqui especificamos o RabbitMQ Server
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            //cria a conexão usando connection factory
+            var connection = factory.CreateConnection();
+
+            using var chanel = connection.CreateModel();
+
+
+            chanel.QueueDeclare("product", exclusive: false);
+            
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            chanel.BasicPublish(exchange: "", routingKey: "product", body: body);
+
+
+        }
+    }
+}
---
+++ RabbitMQProductApi/Services/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQProductApi.Data;
+using RabbitMQProductApi.Models;
+
+namespace RabbitMQProductApi.Services
+{
+    public class ProductService : IProductService
+
+    {
+        private readonly DbContextClass _dbContext;
+        public ProductService(DbContextClass dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Product AddProduct(Product product)
+        {
+            var result = _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+            return result.Entity;
+        }
+        public bool DeleteProduct(int id)
+        {
+            var filteredData = _dbContext.Products.Where(x => x.ProductId == id).FirstOrDefault();
+            var result = _dbContext.Remove(filteredData);
+            _dbContext.SaveChanges();
+            return result !=null ? true : false;
+        }
+        public Product GetProductById(int id)
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return _dbContext.Products.Where(x => x.ProductId == id).FirstOrDefault();
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+        public Product GetProductByName(string name)
+        {
+            return _dbContext.Products.Where(x => x.ProductName == name).FirstOrDefault();
+        }
+
+        public IEnumerable<Product> GetProductList()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            var result = _dbContext.Products.Update(product);
+            _dbContext.SaveChanges();
+            return result.Entity;
+        }
+    }
+}
